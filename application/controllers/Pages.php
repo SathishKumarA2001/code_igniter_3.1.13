202d7c11dce6260c6fc58a587b6e97f2df25ab69
sathish-kumar-7777 <110592089+sathish-kumar-7777@users.noreply.github.com>
2022-08-06 12:00:26 +0530

    CI_controller func test

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,11 +4,22 @@ class Pages extends CI_Controller {
 
     public function view($page='home'){
         if (!file_exists(APPPATH.'/views/pages/'.$page.'.php')){
-            die("Page not found");
+            die("Couldn't find the page you are looking for");
         }
         $data['title'] = ucfirst($page);
         $this->load->view('templates/header',$data);
         $this->load->view('pages/'.$page,$data);
+        $this->load->view('templates/footer',$data);
+    }
+
+    public function show($page){
+        if(!file_exists(APPPATH.'views/docs/'.$page.'.php')){
+            die("Couldn't find the page you are looking for");
+        }
+        $data['title'] = "Form Validation";
+        $head['data'] = "Testing";
+        $this->load->view('templates/header',$data);
+        $this->load->view('docs/'.$page,$head);
         $this->load->view('templates/footer',$data);
     }
 
